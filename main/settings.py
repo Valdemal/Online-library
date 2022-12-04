07cf75525f39e4d991a435cb4e08b16289c3e936
Vladimir Nekhaenko <59586081+Valdemal@users.noreply.github.com>
2022-12-04 16:46:41 +0300

    Change settings for deployment.

--- main/settings.py
+++ main/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = eval(os.getenv('ALLOWED_HOSTS'))
 
 # Application definition
 
@@ -151,5 +151,5 @@ DJOSER = {
 
 # corsheaders
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ["http://localhost:8081"]
+# Пока так
+CORS_ORIGIN_ALLOW_ALL = True
