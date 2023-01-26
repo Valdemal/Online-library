26ac190778a37446adaaac06e3c90c5790e14cf8
Vladimir Nekhaenko <59586081+Valdemal@users.noreply.github.com>
2023-01-26 16:56:04 +0300

    Added a full entry genres filter

--- library/filters.py
+++ library/filters.py
@@ -1,14 +1,33 @@
 from django_filters import rest_framework as drf_filters
+from django_filters.constants import EMPTY_VALUES
 
 from library.models import Book, Author
 
 
-class CharBaseInFilter(drf_filters.CharFilter, drf_filters.BaseInFilter):
-    pass
+class FullEntryFilter(drf_filters.CharFilter, drf_filters.BaseInFilter):
+    """
+    Фильтр для отношения многие-ко-многим, который проверяет вхождение всех
+    значений фильтра в ManyToManyField значение.
+    """
+
+    def filter(self, qs, value):
+        if value in EMPTY_VALUES:
+            return qs
+
+        if self.distinct:
+            qs = qs.distinct()
+
+        method = self.get_method(qs)
+        res = method(**{self.field_name: value[0]})
+
+        for i in range(1, len(value)):
+            res &= method(**{self.field_name: value[i]})
+
+        return res
 
 
 class BookFilter(drf_filters.FilterSet):
-    genres = CharBaseInFilter(field_name='genres__slug', lookup_expr='in', distinct=True)
+    genres = FullEntryFilter(field_name='genres__slug', distinct=True, label='Жанр')
     year_of_writing = drf_filters.RangeFilter()
     author = drf_filters.CharFilter(field_name='author__slug')
 
@@ -18,7 +37,7 @@ class BookFilter(drf_filters.FilterSet):
 
 
 class AuthorFilter(drf_filters.FilterSet):
-    genres = CharBaseInFilter(field_name='book__genres__slug', lookup_expr='in', distinct=True, label='Жанр')
+    genres = FullEntryFilter(field_name='book__genres__slug', distinct=True, label='Жанр')
 
     class Meta:
         model = Author
