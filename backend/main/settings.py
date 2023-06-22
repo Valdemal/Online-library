e6c82823e5e0a9950bdf48a48e701bc74c367ede
Vovan <vovanex12@gmail.com>
2023-06-22 17:36:45 +0300

    Add pagination

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -216,6 +216,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework_csv.renderers.CSVRenderer',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
 }
 
 DJOSER = {
